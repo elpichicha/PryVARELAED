da0f242905ea85c7f532424d081f10898162e7a5
root <mgiraudo@ies21.edu.ar>
2023-08-28 10:58:07 -0300

    Agregar archivos de proyecto.

---
+++ PryVARELAED/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace PryVARELAED
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.datosDelDesarrolladorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estructurasLinealesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pilaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.listaSimpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDobleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estrucutrasNoLinealesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arbolBinarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baseDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasEnLaBaseDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.repasoDeOperacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemasToolStripMenuItem,
+            this.estructurasLinealesToolStripMenuItem,
+            this.estrucutrasNoLinealesToolStripMenuItem,
+            this.baseDeDatosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemasToolStripMenuItem
+            // 
+            this.sistemasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.datosDelDesarrolladorToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.salirToolStripMenuItem});
+            this.sistemasToolStripMenuItem.Name = "sistemasToolStripMenuItem";
+            this.sistemasToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.sistemasToolStripMenuItem.Text = "Sistemas ";
+            // 
+            // datosDelDesarrolladorToolStripMenuItem
+            // 
+            this.datosDelDesarrolladorToolStripMenuItem.Name = "datosDelDesarrolladorToolStripMenuItem";
+            this.datosDelDesarrolladorToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.datosDelDesarrolladorToolStripMenuItem.Text = "Datos del desarrollador...";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(201, 6);
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // estructurasLinealesToolStripMenuItem
+            // 
+            this.estructurasLinealesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colaToolStripMenuItem,
+            this.pilaToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.listaSimpleToolStripMenuItem,
+            this.listaDobleToolStripMenuItem});
+            this.estructurasLinealesToolStripMenuItem.Name = "estructurasLinealesToolStripMenuItem";
+            this.estructurasLinealesToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.estructurasLinealesToolStripMenuItem.Text = "Estructuras Lineales ";
+            // 
+            // colaToolStripMenuItem
+            // 
+            this.colaToolStripMenuItem.Name = "colaToolStripMenuItem";
+            this.colaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colaToolStripMenuItem.Text = "Cola...";
+            // 
+            // pilaToolStripMenuItem
+            // 
+            this.pilaToolStripMenuItem.Name = "pilaToolStripMenuItem";
+            this.pilaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pilaToolStripMenuItem.Text = "Pila..";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // listaSimpleToolStripMenuItem
+            // 
+            this.listaSimpleToolStripMenuItem.Name = "listaSimpleToolStripMenuItem";
+            this.listaSimpleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaSimpleToolStripMenuItem.Text = "Lista simple";
+            // 
+            // listaDobleToolStripMenuItem
+            // 
+            this.listaDobleToolStripMenuItem.Name = "listaDobleToolStripMenuItem";
+            this.listaDobleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaDobleToolStripMenuItem.Text = "Lista doble";
+            // 
+            // estrucutrasNoLinealesToolStripMenuItem
+            // 
+            this.estrucutrasNoLinealesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arbolBinarioToolStripMenuItem});
+            this.estrucutrasNoLinealesToolStripMenuItem.Name = "estrucutrasNoLinealesToolStripMenuItem";
+            this.estrucutrasNoLinealesToolStripMenuItem.Size = new System.Drawing.Size(139, 20);
+            this.estrucutrasNoLinealesToolStripMenuItem.Text = "Estrucutras no lineales ";
+            // 
+            // arbolBinarioToolStripMenuItem
+            // 
+            this.arbolBinarioToolStripMenuItem.Name = "arbolBinarioToolStripMenuItem";
+            this.arbolBinarioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.arbolBinarioToolStripMenuItem.Text = "Arbol binario..";
+            // 
+            // baseDeDatosToolStripMenuItem
+            // 
+            this.baseDeDatosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem,
+            this.consultasEnLaBaseDeDatosToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.repasoDeOperacionesToolStripMenuItem});
+            this.baseDeDatosToolStripMenuItem.Name = "baseDeDatosToolStripMenuItem";
+            this.baseDeDatosToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.baseDeDatosToolStripMenuItem.Text = "Base de datos";
+            // 
+            // operacionConTablasDeBasesDeDatosToolStripMenuItem
+            // 
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem.Name = "operacionConTablasDeBasesDeDatosToolStripMenuItem";
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem.Size = new System.Drawing.Size(291, 22);
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem.Text = "Operacion con tablas de bases de datos...";
+            this.operacionConTablasDeBasesDeDatosToolStripMenuItem.Click += new System.EventHandler(this.operacionConTablasDeBasesDeDatosToolStripMenuItem_Click);
+            // 
+            // consultasEnLaBaseDeDatosToolStripMenuItem
+            // 
+            this.consultasEnLaBaseDeDatosToolStripMenuItem.Name = "consultasEnLaBaseDeDatosToolStripMenuItem";
+            this.consultasEnLaBaseDeDatosToolStripMenuItem.Size = new System.Drawing.Size(291, 22);
+            this.consultasEnLaBaseDeDatosToolStripMenuItem.Text = "Consultas en la base de datos...";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(288, 6);
+            // 
+            // repasoDeOperacionesToolStripMenuItem
+            // 
+            this.repasoDeOperacionesToolStripMenuItem.Name = "repasoDeOperacionesToolStripMenuItem";
+            this.repasoDeOperacionesToolStripMenuItem.Size = new System.Drawing.Size(291, 22);
+            this.repasoDeOperacionesToolStripMenuItem.Text = "Repaso de operaciones...";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmPrincipal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem datosDelDesarrolladorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estructurasLinealesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pilaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem listaSimpleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDobleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estrucutrasNoLinealesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arbolBinarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem baseDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operacionConTablasDeBasesDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasEnLaBaseDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem repasoDeOperacionesToolStripMenuItem;
+    }
+}
+
---
+++ PryVARELAED/Form2.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PryVARELAED
+{
+    partial class FRMcola
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMcola));
+            this.MRCnuevoelemento = new System.Windows.Forms.GroupBox();
+            this.LBLcodigo = new System.Windows.Forms.Label();
+            this.TXTcodigo = new System.Windows.Forms.TextBox();
+            this.LBLnombre = new System.Windows.Forms.Label();
+            this.LBLtramite = new System.Windows.Forms.Label();
+            this.TXTnombre = new System.Windows.Forms.TextBox();
+            this.TXTtramite = new System.Windows.Forms.TextBox();
+            this.BTNagregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BTNeliminar = new System.Windows.Forms.Button();
+            this.TXTtramite2 = new System.Windows.Forms.TextBox();
+            this.TXTnombre2 = new System.Windows.Forms.TextBox();
+            this.LBLtramite2 = new System.Windows.Forms.Label();
+            this.LBLnombre2 = new System.Windows.Forms.Label();
+            this.TXTcodigo2 = new System.Windows.Forms.TextBox();
+            this.LBLcodigo2 = new System.Windows.Forms.Label();
+            this.MRClistadoenunalistaygrilla = new System.Windows.Forms.GroupBox();
+            this.LSTcola = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MRCnuevoelemento.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.MRClistadoenunalistaygrilla.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MRCnuevoelemento
+            // 
+            this.MRCnuevoelemento.Controls.Add(this.BTNagregar);
+            this.MRCnuevoelemento.Controls.Add(this.TXTtramite);
+            this.MRCnuevoelemento.Controls.Add(this.TXTnombre);
+            this.MRCnuevoelemento.Controls.Add(this.LBLtramite);
+            this.MRCnuevoelemento.Controls.Add(this.LBLnombre);
+            this.MRCnuevoelemento.Controls.Add(this.TXTcodigo);
+            this.MRCnuevoelemento.Controls.Add(this.LBLcodigo);
+            this.MRCnuevoelemento.Location = new System.Drawing.Point(255, 52);
+            this.MRCnuevoelemento.Name = "MRCnuevoelemento";
+            this.MRCnuevoelemento.Size = new System.Drawing.Size(191, 163);
+            this.MRCnuevoelemento.TabIndex = 1;
+            this.MRCnuevoelemento.TabStop = false;
+            this.MRCnuevoelemento.Text = "Nuevo Elemento";
+            // 
+            // LBLcodigo
+            // 
+            this.LBLcodigo.AutoSize = true;
+            this.LBLcodigo.Location = new System.Drawing.Point(6, 31);
+            this.LBLcodigo.Name = "LBLcodigo";
+            this.LBLcodigo.Size = new System.Drawing.Size(40, 13);
+            this.LBLcodigo.TabIndex = 0;
+            this.LBLcodigo.Text = "Codigo";
+            // 
+            // TXTcodigo
+            // 
+            this.TXTcodigo.Location = new System.Drawing.Point(80, 32);
+            this.TXTcodigo.Name = "TXTcodigo";
+            this.TXTcodigo.Size = new System.Drawing.Size(80, 20);
+            this.TXTcodigo.TabIndex = 1;
+            // 
+            // LBLnombre
+            // 
+            this.LBLnombre.AutoSize = true;
+            this.LBLnombre.Location = new System.Drawing.Point(6, 68);
+            this.LBLnombre.Name = "LBLnombre";
+            this.LBLnombre.Size = new System.Drawing.Size(44, 13);
+            this.LBLnombre.TabIndex = 2;
+            this.LBLnombre.Text = "Nombre";
+            // 
+            // LBLtramite
+            // 
+            this.LBLtramite.AutoSize = true;
+            this.LBLtramite.Location = new System.Drawing.Point(6, 109);
+            this.LBLtramite.Name = "LBLtramite";
+            this.LBLtramite.Size = new System.Drawing.Size(42, 13);
+            this.LBLtramite.TabIndex = 3;
+            this.LBLtramite.Text = "Tramite";
+            // 
+            // TXTnombre
+            // 
+            this.TXTnombre.Location = new System.Drawing.Point(78, 68);
+            this.TXTnombre.Name = "TXTnombre";
+            this.TXTnombre.Size = new System.Drawing.Size(82, 20);
+            this.TXTnombre.TabIndex = 4;
+            // 
+            // TXTtramite
+            // 
+            this.TXTtramite.Location = new System.Drawing.Point(78, 106);
+            this.TXTtramite.Name = "TXTtramite";
+            this.TXTtramite.Size = new System.Drawing.Size(82, 20);
+            this.TXTtramite.TabIndex = 5;
+            // 
+            // BTNagregar
+            // 
+            this.BTNagregar.Location = new System.Drawing.Point(74, 132);
+            this.BTNagregar.Name = "BTNagregar";
+            this.BTNagregar.Size = new System.Drawing.Size(85, 21);
+            this.BTNagregar.TabIndex = 6;
+            this.BTNagregar.Text = "Agregar";
+            this.BTNagregar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BTNeliminar);
+            this.groupBox1.Controls.Add(this.TXTtramite2);
+            this.groupBox1.Controls.Add(this.TXTnombre2);
+            this.groupBox1.Controls.Add(this.LBLtramite2);
+            this.groupBox1.Controls.Add(this.LBLnombre2);
+            this.groupBox1.Controls.Add(this.TXTcodigo2);
+            this.groupBox1.Controls.Add(this.LBLcodigo2);
+            this.groupBox1.Location = new System.Drawing.Point(462, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 163);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nuevo Elemento";
+            // 
+            // BTNeliminar
+            // 
+            this.BTNeliminar.Location = new System.Drawing.Point(74, 132);
+            this.BTNeliminar.Name = "BTNeliminar";
+            this.BTNeliminar.Size = new System.Drawing.Size(85, 21);
+            this.BTNeliminar.TabIndex = 6;
+            this.BTNeliminar.Text = "Eliminar";
+            this.BTNeliminar.UseVisualStyleBackColor = true;
+            // 
+            // TXTtramite2
+            // 
+            this.TXTtramite2.Location = new System.Drawing.Point(78, 106);
+            this.TXTtramite2.Name = "TXTtramite2";
+            this.TXTtramite2.Size = new System.Drawing.Size(82, 20);
+            this.TXTtramite2.TabIndex = 5;
+            // 
+            // TXTnombre2
+            // 
+            this.TXTnombre2.Location = new System.Drawing.Point(78, 68);
+            this.TXTnombre2.Name = "TXTnombre2";
+            this.TXTnombre2.Size = new System.Drawing.Size(82, 20);
+            this.TXTnombre2.TabIndex = 4;
+            // 
+            // LBLtramite2
+            // 
+            this.LBLtramite2.AutoSize = true;
+            this.LBLtramite2.Location = new System.Drawing.Point(6, 109);
+            this.LBLtramite2.Name = "LBLtramite2";
+            this.LBLtramite2.Size = new System.Drawing.Size(42, 13);
+            this.LBLtramite2.TabIndex = 3;
+            this.LBLtramite2.Text = "Tramite";
+            // 
+            // LBLnombre2
+            // 
+            this.LBLnombre2.AutoSize = true;
+            this.LBLnombre2.Location = new System.Drawing.Point(6, 68);
+            this.LBLnombre2.Name = "LBLnombre2";
+            this.LBLnombre2.Size = new System.Drawing.Size(44, 13);
+            this.LBLnombre2.TabIndex = 2;
+            this.LBLnombre2.Text = "Nombre";
+            // 
+            // TXTcodigo2
+            // 
+            this.TXTcodigo2.Location = new System.Drawing.Point(80, 32);
+            this.TXTcodigo2.Name = "TXTcodigo2";
+            this.TXTcodigo2.Size = new System.Drawing.Size(80, 20);
+            this.TXTcodigo2.TabIndex = 1;
+            // 
+            // LBLcodigo2
+            // 
+            this.LBLcodigo2.AutoSize = true;
+            this.LBLcodigo2.Location = new System.Drawing.Point(6, 31);
+            this.LBLcodigo2.Name = "LBLcodigo2";
+            this.LBLcodigo2.Size = new System.Drawing.Size(40, 13);
+            this.LBLcodigo2.TabIndex = 0;
+            this.LBLcodigo2.Text = "Codigo";
+            // 
+            // MRClistadoenunalistaygrilla
+            // 
+            this.MRClistadoenunalistaygrilla.Controls.Add(this.LSTcola);
+            this.MRClistadoenunalistaygrilla.Location = new System.Drawing.Point(16, 224);
+            this.MRClistadoenunalistaygrilla.Name = "MRClistadoenunalistaygrilla";
+            this.MRClistadoenunalistaygrilla.Size = new System.Drawing.Size(626, 167);
+            this.MRClistadoenunalistaygrilla.TabIndex = 8;
+            this.MRClistadoenunalistaygrilla.TabStop = false;
+            this.MRClistadoenunalistaygrilla.Text = "Listado en un lista y grilla";
+            // 
+            // LSTcola
+            // 
+            this.LSTcola.FormattingEnabled = true;
+            this.LSTcola.Location = new System.Drawing.Point(6, 23);
+            this.LSTcola.Name = "LSTcola";
+            this.LSTcola.Size = new System.Drawing.Size(127, 121);
+            this.LSTcola.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 170);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FRMcola
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.MRClistadoenunalistaygrilla);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MRCnuevoelemento);
+            this.Name = "FRMcola";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COLA";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MRCnuevoelemento.ResumeLayout(false);
+            this.MRCnuevoelemento.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.MRClistadoenunalistaygrilla.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox MRCnuevoelemento;
+        private System.Windows.Forms.Button BTNagregar;
+        private System.Windows.Forms.TextBox TXTtramite;
+        private System.Windows.Forms.TextBox TXTnombre;
+        private System.Windows.Forms.Label LBLtramite;
+        private System.Windows.Forms.Label LBLnombre;
+        private System.Windows.Forms.TextBox TXTcodigo;
+        private System.Windows.Forms.Label LBLcodigo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BTNeliminar;
+        private System.Windows.Forms.TextBox TXTtramite2;
+        private System.Windows.Forms.TextBox TXTnombre2;
+        private System.Windows.Forms.Label LBLtramite2;
+        private System.Windows.Forms.Label LBLnombre2;
+        private System.Windows.Forms.TextBox TXTcodigo2;
+        private System.Windows.Forms.Label LBLcodigo2;
+        private System.Windows.Forms.GroupBox MRClistadoenunalistaygrilla;
+        private System.Windows.Forms.ListBox LSTcola;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
