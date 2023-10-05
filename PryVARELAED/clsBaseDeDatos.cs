8c3af043c7f0d89aa95043a021ea5607d3d2839c
root <mgiraudo@ies21.edu.ar>
2023-10-05 11:43:45 -0300

    05/10(23

---
+++ PryVARELAED/clsBaseDeDatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace PryVARELAED
+{
+    internal class clsBaseDeDatos
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comadno = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string cadenaconexion = "provider=Microsoft.jet.OLEDB.4.0;DATA Source=libreria.mdb";
+        private string varCadenadaConexion = "provide
+    }
+}
